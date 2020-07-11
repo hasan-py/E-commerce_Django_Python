3523dba830eaeea92d2b89a9b02e505a613e9d96
sajib1066 <sajib1066@gmail.com>
2020-07-11 17:42:04 +0600

    add date fields on product model

--- store/models/product.py
+++ store/models/product.py
@@ -7,6 +7,7 @@ class Product(models.Model):
 	category = models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
 	description = models.TextField()
 	image = models.ImageField(upload_to='upload/products')
+	date = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.name
